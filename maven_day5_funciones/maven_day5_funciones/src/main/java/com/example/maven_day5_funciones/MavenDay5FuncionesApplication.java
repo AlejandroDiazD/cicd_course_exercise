33a5800ed01f8746b6729df90794f0bbce927699
AlejandroDiazD <adidi.diaz@gmail.com>
2024-09-20 12:20:09 +0200

    Add maven project for functions

---
+++ maven_day5_funciones/maven_day5_funciones/src/main/java/com/example/maven_day5_funciones/MavenDay5FuncionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_day5_funciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDay5FuncionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDay5FuncionesApplication.class, args);
+	}
+
+}
---
+++ maven_day5_funciones/maven_day5_funciones/src/test/java/com/example/maven_day5_funciones/MavenDay5FuncionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_day5_funciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDay5FuncionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
